ed6603dc36d88fc6fec1cab42418e63bcdf809cc
Przemek <22966404+ppudol@users.noreply.github.com>
2024-02-27 17:45:41 +0100

    return calendar data in api point

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\CalendarFileFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
  #[Route('/api', name: 'app_api')]
 class ApiController extends AbstractController
 {
-    public function __construct()
+    public function __construct(private CalendarFileFetcher $calendarFileFetcher)
     {}
     
     #[Route('/get-calendar-data', name: 'app_api_get_calendar_data', methods: ['GET'])]
     public function getCalendarData(): JsonResponse
     {
+        $this->calendarFileFetcher->getCalendarData();
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiController.php',
+            $this->calendarFileFetcher->getCalendarData()
         ]);
     }
 
